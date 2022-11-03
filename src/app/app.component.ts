5dfe39780006957f14b3cbe10b6d161dec598d3b
felipealvarez23 <felipealvarezrincon@gmail.com>
2022-11-02 23:00:05 -0500

    Base del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewChild } from '@angular/core';
+import { JsonEditorOptions } from 'ang-jsoneditor';
+import { sign } from 'jsonwebtoken';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public editorOptions: JsonEditorOptions;
+  public initialData: any;
+  public visibleData: any;
+  public privateKey = '';
+
+  constructor() {
+    this.editorOptions = new JsonEditorOptions();
+    this.editorOptions.mode = 'code';
+    this.initialData = {
+      iat: 1648075435,
+      meta: {
+        messageId: '31211565-6905-3cb3-8d27-3fea93c27b1d',
+        requestDate: 20150625200000,
+        systemId: 'AW78461',
+        usrMod: 'BIZAGI',
+        version: 1,
+        responseUrl: '',
+        urlCancel: '',
+      },
+      start: {
+        productId: '8',
+        subProductCategory: '8001',
+        channelId: '002',
+        businessLineId: '002',
+      },
+    };
+    this.visibleData = this.initialData;
+  }
+
+  showJson(d: Event): void {
+    this.visibleData = d;
+  }
+
+  generateJwt(): void {
+    console.log(
+      sign(this.initialData, this.privateKey, {
+        expiresIn: '500s',
+        algorithm: 'RS256',
+      })
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NgJsonEditorModule } from 'ang-jsoneditor';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, NgJsonEditorModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
